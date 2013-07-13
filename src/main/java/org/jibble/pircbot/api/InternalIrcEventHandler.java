5615c6d9b5e550de17b10b4c18e81e8f7bd63733
OndraZizka <zizka@seznam.cz>
2013-07-13 14:13:45 +0200

    Remove redundant comments.

--- src/main/java/org/jibble/pircbot/api/InternalIrcEventHandler.java
+++ src/main/java/org/jibble/pircbot/api/InternalIrcEventHandler.java
@@ -39,84 +39,55 @@ public class InternalIrcEventHandler implements IIrcEventHandler {
 
     @Override public void onDeVoice(String channel, String sourceNick, String sourceLogin, String sourceHostname, String recipient) {}
 
-
     @Override public void onSetChannelKey(String channel, String sourceNick, String sourceLogin, String sourceHostname, String key) {}
-
 
     @Override public void onRemoveChannelKey(String channel, String sourceNick, String sourceLogin, String sourceHostname, String key) {}
 
-
     @Override public void onSetChannelLimit(String channel, String sourceNick, String sourceLogin, String sourceHostname, int limit) {}
-
 
     @Override public void onRemoveChannelLimit(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
-
     @Override public void onSetChannelBan(String channel, String sourceNick, String sourceLogin, String sourceHostname, String hostmask) {}
-
 
     @Override public void onRemoveChannelBan(String channel, String sourceNick, String sourceLogin, String sourceHostname, String hostmask) {}
 
-
     @Override public void onSetTopicProtection(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
-
 
     @Override public void onRemoveTopicProtection(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
-
     @Override public void onSetNoExternalMessages(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
-
 
     @Override public void onRemoveNoExternalMessages(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
-
     @Override public void onSetInviteOnly(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
-
     @Override public void onRemoveInviteOnly(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
-
 
     @Override public void onSetModerated(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
     @Override public void onRemoveModerated(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
-
     @Override public void onSetPrivate(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
-
 
     @Override public void onRemovePrivate(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
-
     @Override public void onSetSecret(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
 
-
     @Override public void onRemoveSecret(String channel, String sourceNick, String sourceLogin, String sourceHostname) {}
-
-
     
     @Override public void onInvite(String targetNick, String sourceNick, String sourceLogin, String sourceHostname, String channel)  {}
-
-
     
     @Override public void onDccSendRequest(String sourceNick, String sourceLogin, String sourceHostname, String filename, long address, int port, int size) {}
-
-
     
     @Override public void onDccChatRequest(String sourceNick, String sourceLogin, String sourceHostname, long address, int port) {}
-
-
     
     @Override public void onIncomingFileTransfer(DccFileTransfer transfer) {}
-
-
     
     @Override public void onFileTransferFinished(DccFileTransfer transfer, Exception e) {}
-
-
     
     @Override public void onIncomingChatRequest(DccChat chat) {}
 
-
+    
     /**
      * This method is called whenever we receive a VERSION request.
      * This abstract implementation responds with the PircBot's _version string,
@@ -202,33 +173,32 @@ public class InternalIrcEventHandler implements IIrcEventHandler {
     
     @Override public void onUnknown(String line) {}
 
-    public void onServerResponse(int code, String response) {}
+    @Override public void onServerResponse(int code, String response) {}
     
-    public void onUserList(String channel, User[] users) {}
+    @Override public void onUserList(String channel, User[] users) {}
     
-    public void onMessage(String channel, String sender, String login, String hostname, String message) {}
+    @Override public void onMessage(String channel, String sender, String login, String hostname, String message) {}
     
-    public void onPrivateMessage(String sender, String login, String hostname, String message) {}
+    @Override public void onPrivateMessage(String sender, String login, String hostname, String message) {}
     
-    public void onAction(String sender, String login, String hostname, String target, String action) {}
+    @Override public void onAction(String sender, String login, String hostname, String target, String action) {}
     
-    public void onNotice(String sourceNick, String sourceLogin, String sourceHostname, String target, String notice) {}
+    @Override public void onNotice(String sourceNick, String sourceLogin, String sourceHostname, String target, String notice) {}
     
-    public void onJoin(String channel, String sender, String login, String hostname) {}
+    @Override public void onJoin(String channel, String sender, String login, String hostname) {}
     
-    public void onPart(String channel, String sender, String login, String hostname) {}
+    @Override public void onPart(String channel, String sender, String login, String hostname) {}
 
-    public void onNickChange(String oldNick, String login, String hostname, String newNick) {}
-
+    @Override public void onNickChange(String oldNick, String login, String hostname, String newNick) {}
     
-    public void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {}
+    @Override public void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {}
     
-    public void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {}
+    @Override public void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {}
     
-    public void onTopic(String channel, String topic) {}
+    @Override public void onTopic(String channel, String topic) {}
 
-    public void onTopic(String channel, String topic, String setBy, long date, boolean changed) {}
+    @Override public void onTopic(String channel, String topic, String setBy, long date, boolean changed) {}
 
-    public void onChannelInfo(String channel, int userCount, String topic) {}
+    @Override public void onChannelInfo(String channel, int userCount, String topic) {}
 
 }// interface
